6995618d94975ba35172484eb2539c4efb5fa619
muyun <wenlzhao@gmail.com>
2013-08-30 00:55:52 +0800

    stack data structure based on array and linked list

--- src/Postfix.cpp
+++ src/Postfix.cpp
@@ -1,4 +1,28 @@
 //Postfix.cpp
 //
 #include <iostream>
-#include "datastructure"
+#include <string.h>
+#include "stack.cpp"
+
+int main(int argc, char *argv[])
+{
+    char *a = argv[1];
+    int N = strlen(a);
+
+    //STACK<char> ops(N);
+    LinkList<char> ops;
+    
+
+    for (int i=0; i<N; i++)
+    {
+        if (a[i] == ')')
+            cout<<ops.pop()<< " ";
+        if ((a[i] == '+') || (a[i] == '*'))
+            ops.push(a[i]);
+        if ((a[i] >= '0') && (a[i] <= '9'))
+            cout << a[i] << " ";
+    }
+    cout << endl;
+    
+    return 0;
+}
--- src/stack.cpp
+++ src/stack.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 using namespace std;
 
+//based on array
 template <class Item>
 class STACK
 {
@@ -13,6 +14,7 @@ public:
         s = new Item[max];
         N = 0;
     }
+    //STACK(const Item&);
 
     int isEmpty() const { return N == 0; }
 
@@ -24,5 +26,44 @@ private:
     int N;  // index
 };
 
+//based on linked list
+template <typename Item>
+class LinkList
+{
+private:
+    struct node
+    {
+        Item item;
+        node* next;
+        node(Item x, node* t): item(x), next(t){};
+        
+    };
+    
+    typedef node* link;
+    link head; //pointer head points to the first item
+    
+public:
+    LinkList(){ head = 0;}
+    int isEmpty() const { return head == 0;}
+    
+    //push a new item at the front of the list, head points to this item
+    void push(Item x)
+    {
+        // old head is given to the new node
+        // and the new node is given to head
+        head = new node(x, head); 
+    } 
 
+    //pop the item at the front of the list
+    Item pop()
+    {
+        Item v = head->item;
+        link t = head->next;
+        delete head;
+        head = t;
 
+        return v;
+    }
+    
+};
+    
