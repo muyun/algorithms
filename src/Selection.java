b68c9d6285ef7ccb699f44a7e4159419abc2de95
muyun <wenlzhao@gmail.com>
2013-09-15 22:03:23 +0800

    add the test sort class and update Selectionsort class

--- src/Selection.java
+++ src/Selection.java
@@ -1,17 +1,21 @@
-// Sort.java: wenlong
-// Description: Bubble sort, Selection sort, Insertion sort and ShellSort
-//
+// Selection.java: wenlong
+// Description: Selection sort
 // Assume: 1) total order (like v.compareTo(w))
 //         2)callback= reference to executable code
 //              * client passes array of objects to sort() function
 //              * The sort() function calls back object's compareTo() method as needed
 //                   ---user-defined comparable types implement the comparable interface
 //
-// bubble sort
-//Selection Sort improves on the bubble sort by reducing the number of swaps necessary
+// Performance: 1) selection Sort improves on the bubble sort by reducing the number of swaps necessary
 // from O(N*N) to O(N). Unfortunately, the number of comparisons remains O(N*N) .
+//              2) uses (N-1)+ (N-2)+ ... + 1 + 0 ~ N2/2 compares and N exchanges
 //
-
+//              3) Running time is insensitive to input: the process of finding the smallest item on one pass
+//                 through the array doesnot give much information about where the smallest item might be on the next pass
+//              4) Data movement is minimal: Each of the N exchanges changes the value of two array entries,
+//                 which means that the number of array accesses is a linear function of the array size N.
+//
+//---------------------------------------------------------------------------------------------------
 //import java.util.Comparator;
 /*
   // Comparable interface built in Java
@@ -30,8 +34,8 @@ class Bubble
         //find the remaining biggest in every loop
         for(int out=N-1; out>1; out--) // outer loop is backward
             for(int in=0; in<out; in++) // inner loop is forward
-                if (a[in]>a[in+1])
-                    swap(a[in],a[in+1]);
+                if ( less(a[in+1],a[in]) )
+                    swap(a, in, in+1);
     }
 
     //exchange a[i] and a[j]
@@ -40,6 +44,11 @@ class Bubble
         Comparable temp = a[i];
         a[i] = a[j];
         a[j] = temp;
+    }
+
+    private static boolean less(Comparable v, Comparable w)
+    {
+        return v.compareTo(w) < 0;
     }
 
 }
@@ -53,7 +62,7 @@ public class Selection
         int N = a.length;
         for(int i=0; i<N; i++){
             //exchange a[i] with smallest entry in a[1+1...N]
-            int min = i
+            int min = i;
             for(int j=i+1;j<N;j++) //assume i is the min entry
                 if(less(a[j],a[min]))
                     min = j;
@@ -74,7 +83,7 @@ public class Selection
         a[j] = temp;
     }
 
-    private static void display(Comaprable[] a)
+    public static void display(Comparable[] a)
     {
         for(int i=0; i<a.length; i++)
             StdOut.print(a[i] + " ");
@@ -90,7 +99,15 @@ public class Selection
     }
 
     //test
+    public static void main(String[] args)
+    {
+        String[] a = StdIn.readStrings();
+        
+        sort(a);
+        assert isSorted(a);
+        display(a);
+        
+    }
     
 }
-
 
