7ffd1611cf614a41054b3d6e00b4e6ee9188c22c
muyun <wenlzhao@gmail.com>
2013-09-24 15:20:35 +0800

    update

--- src/Merge.java
+++ src/Merge.java
@@ -1,9 +1,10 @@
-// Top-down mergesort
-//Merge.java is a recursive mergesort implementation based on the abstract in-place merge
-// mergesort uses at most NlgN compares and 6NlgN array accesses to sort any array of size N
-
-import java.util.Comparator;
-
+//Merge.java: wenlong
+//Description: Top-down mergesort is a recursive mergesort implementation
+//
+//Performance:
+//at most NlgN compares and 6NlgN (2N for the copy, 2N for the move back, and at most 2N for compares) array accesses 
+//
+//---------------------------------------------------------------------
 public class Merge
 {
     // use static when we want to provide class level access to a method,
@@ -21,19 +22,42 @@ public class Merge
     private static void sort(Comparable[] a, Comparable[] aux, int lo, int hi)
     {
         if (hi <= lo) return;
+        //TODO: Mergesort has too much overhead for tiny subarrays
+        //      use inertion sort for small subarrays
+        // if(hi <= lo + CUTOFF -1 ) Insertion.sort(a, lo, hi)
+        
+        //recursive calls, stack operations
+        // push the local variable like mid into the stack of the method implicitly
         int mid = lo + (hi - lo) / 2;
+        //StdOut.println(lo + " " + mid + " " + hi);
+        
         sort(a, aux, lo, mid);
         sort(a, aux, mid+1, hi);
+        //stop if already sorted
+        // is biggest item in first half <= smallest item in second half
+        if(!less(a[mid+1], a[mid]))
+            return;
         merge(a, aux, lo, mid, hi);
+        
+        //StdOut.println("(" + lo + "," + mid + "," + hi + ")");
+        
     }
 
     public static void merge(Comparable[] a, Comparable[] aux, int lo, int mid, int hi)
     {
         //precondition
+        //Assertion: Statement to test assumptions about your program
+        //           helps detect logic bugs and Documents code
+        //can enable or disable at runtime
+        //java -ea MyProgram --enable assertions
+        //java -da MyProgram --diable assertions (default)
         assert isSorted(a, lo, mid);
         assert isSorted(a, mid+1, hi);
 
         // copy to aux[]
+        //TODO:eliminate the copy to the auxiliary array
+        //copy to the auxiliary array used for merging
+        //switch the role of the input and auxiliary array
         for (int k = lo; k <= hi; k++)
             aux[k] = a[k];
 
@@ -75,10 +99,11 @@ public class Merge
         return true;
     }
 
-    private static void show(Comparable[] a)
+    private static void display(Comparable[] a)
     {
         for (int i = 0; i < a.length; i++)
-            StdOut.println(a[i]);
+            StdOut.print(a[i] + " ");
+        StdOut.println();
     }
     
     // test
@@ -86,7 +111,7 @@ public class Merge
     {
         String[] a = StdIn.readStrings();
         Merge.sort(a);
-        show(a);
+        display(a);
         
     }
     
